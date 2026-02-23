dc2146520d78bc9ebdac4f51f491ea487c8f3e48
prajeesh <prajeeshag@gmail.com>
2026-02-23 19:49:59 +0530

    add Readme

--- scripts/render.sh
+++ scripts/render.sh
@@ -5,3 +5,4 @@ rm -f Prajeesh_Ag_CV.yaml
 uv run python scripts/create_publication_list.py
 uv run rendercv render Prajeesh_Ag_CV.yaml
 mv rendercv_output/Prajeesh_Ag_CV.pdf Prajeesh_Ag_CV.pdf
+rm Prajeesh_Ag_CV.yaml
